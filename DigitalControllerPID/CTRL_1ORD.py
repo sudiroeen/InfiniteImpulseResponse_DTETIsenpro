e46a21074a0a696202d3b9980a747620e15dba79
Sudiro <44321165+sudiroeen@users.noreply.github.com>
2025-03-15 14:38:34 +0700

    Update CTRL_1ORD.py

--- DigitalControllerPID/CTRL_1ORD.py
+++ DigitalControllerPID/CTRL_1ORD.py
@@ -6,63 +6,93 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-param = (1.01, 0.8)
+def vietaformula(poles):
+	if len(poles) == 1:
+		return np.array([[1,-poles[0]]]).T 
+	else:
+		seedvec = vietaformula([poles[0]])
+		for i in range(1,len(poles)):
+			Idt = np.eye(i+1)
+			Ozr = np.zeros((1,i+1))
+			matm = np.vstack([Idt, Ozr]) + np.vstack([Ozr, -poles[i]*Idt])
+			seedvec = matm @ seedvec
+		return np.real(seedvec)
 
-def system_DUNSTABLE_1ord(vec_yn, vec_xn):
-	# vec_yn = [y[n], y[n-1], y[n-2]]
-	# vec_xn = [x[n], x[n-1], x[n-2]]
-	wc = 2*np.pi*10
-	Ts = 1e-3
-	a1 = param[0]
-	b1 = param[1]
-	for i in range(1,len(vec_yn)):
-		vec_yn[i] = vec_yn[i-1]
-	vec_yn[0] = a1*vec_yn[1] + b1*vec_xn[1]
-	return  vec_yn[0]
+Ts = 1e-3
+paramd = (1.01, -0.01)
 
-def controller_DUNSTABLE_1ord(vec_en, vec_cn, param):
-	# vec_en = [e[n], e[n-1], e[n-2]]
-	# vec_cn = [c[n], c[n-1], c[n-2]]
-	# param = [a,b]
-	apar, bpar = param
-	for i in range(1,len(vec_cn)):
-		vec_cn[i] = vec_cn[i-1]
-		vec_en[i] = vec_en[i-1]
-	Ts = 1e-3
-	Kp = (apar + 0.1)/bpar
-	Td = -0.1/(apar + 0.1)
-	Ti = (apar + 0.1)/(0.59 + 2*Td*(apar + 0.1)) * Ts
-	vec_cn[0] = vec_cn[1] + Kp*(1+Td)*vec_en[0] + Kp*(Ts/Ti - 1 - 2*Td)*vec_en[1] + Kp*Td*vec_en[2]
-	return vec_cn[0]
+poles = [0.95+1j*0., 0.9-1j*0., 0.9]
+# poles = [0.1+1j*0.9, 0.1-1j*0.9, 0.9]
+_, q2, q1, q0 = vietaformula(poles)
+apar, bpar = paramd
+# print((q2[0],q1[0], q0[0]))
+bt0des = (q2[0] + apar + 1)/bpar
+bt1des = (q1[0] - apar)/bpar
+bt2des = q0[0]/bpar
 
-def ref_sig(t):
-	return 1 * np.sin(2*np.pi*t*1) + 0.2*np.sin(2*np.pi*t*100)
+def thesystem(ynm1, cnm1):
+	a1, b1 = paramd
+	ynm0 = a1*ynm1 + b1*cnm1
+	return  ynm0
+
+def thecontroller(enm0, enm1, enm2, cnm1):
+	cnm0 = cnm1 + bt0des*enm0 + bt1des*enm1 + bt2des*enm2
+	return cnm0
+
+def square_sig(ctr, pct):
+	# return np.sin(2*np.pi*t) #, counter
+	if(ctr<pct): 
+		ctr += 1
+		return 1,ctr 
+	elif ctr < 1000:
+		ctr+=1
+		return -2, ctr 
+	else:
+		ctr = 0 
+		return -2, ctr 
+
 
 def controller_OPENLOOP(ref):
 	return 1*ref 
 
-yout = list()
-err = list()
-cinp = list()
+ynlist = list()
+rnlist = list()
+cnlist = list()
+enlist = list()
 Ts = 1e-3
-tm = np.arange(0, 5, step=Ts)
+tm = np.arange(0, 2, step=Ts)
+counter = 0 
+cnm0, cnm1, cnm2 = [0,0,0]
+enm0, enm1, enm2 = [0,0,0]
+ynm0, ynm1 = [0,0]
+rnm0, rnm1 = [0,0]
 
 for t in tm:
-	if len(yout) < 3:
-		yout.append(0)
-		err.append(0)
-		cinp.append(0)
-		continue
+	if len(ynlist) < 3:
+		ynlist.append(0)
+		rnlist.append(0)
+		cnlist.append(0)
+		enlist.append(0)
 	else:
-		yout_ = system_DUNSTABLE_1ord(yout[-3:], cinp[-3:])
-		ref_ = ref_sig(t)
-		err_ = ref_ - yout_
-		cinp_ = controller_DUNSTABLE_1ord(yout[-3:], cinp[-3:], param)
-		err.append(err_)
-		yout.append(yout_)
-		cinp.append(ref_)
+		ynm1 = np.copy(ynm0)
+		rnm0, counter = square_sig(counter, 500)
+		enm2 = np.copy(enm1)
+		enm1 = np.copy(enm0)
+
+		cnm2 = np.copy(cnm1)
+		cnm1 = np.copy(cnm0)
+
+		ynm0 = thesystem(ynm1, cnm1)
+
+		enm0 = rnm0 - ynm0
+		# cnm0 = controller_OPENLOOP(rnm0)
+		cnm0 = thecontroller(enm0, enm1, enm2, cnm1)
+		rnlist.append(rnm0)
+		ynlist.append(ynm0)
+		cnlist.append(cnm0)
+		enlist.append(enm0)
 
 fig, ax = plt.subplots(2)
-ax[0].plot(tm, cinp, '-k', tm, yout, '-r')
-ax[1].plot(tm, cinp, '-k', tm, yout, '-r')
+ax[0].plot(tm, rnlist, '-k', tm, ynlist, '-r')
+ax[1].plot(tm, cnlist, '-k')
 plt.show()
